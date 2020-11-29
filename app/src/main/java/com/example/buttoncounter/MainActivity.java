33e52e4afb71abeb5d26f234406f1787fad5c8c1
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2020-11-29 03:23:31 -0800

    Override methods for minichallenge

--- app/src/main/java/com/example/buttoncounter/MainActivity.java
+++ app/src/main/java/com/example/buttoncounter/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.buttoncounter;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +18,11 @@ public class MainActivity extends AppCompatActivity {
     // EditText, saying this is type of info.
 
     private TextView textView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -47,6 +51,32 @@ public class MainActivity extends AppCompatActivity {
         if (button != null) {
             button.setOnClickListener(ourOnClickListener);
         }
+        Log.d(TAG, "onCreate: out");
 
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
